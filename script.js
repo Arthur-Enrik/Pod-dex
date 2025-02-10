9c3d1bcc977366e7ace15e4a641014b28aab246c
Arthur Enrik <arthurenrick2008@gmail.com>
2025-02-10 10:30:53 -0300

    V1.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Selecionando elementos
-const Input = document.querySelector('.input_search');
+const input = document.querySelector('input.input_search');
 const Button_Prev = document.querySelector('.btn_prev');
 const Button_Next = document.querySelector('.btn_next');
 const display = document.querySelector('.pokemon_image');
@@ -8,13 +8,15 @@ const PokemonID_display = document.querySelector('.pokemon_id')
 const image_url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"
 
 //Eventos
-Button_Next.onclick = function() {GetInfoForPokemons('next')};
-Button_Prev.onclick = function() {GetInfoForPokemons('prev')};
+Button_Next.onclick = function() {GetInfoSequential('next')};
+Button_Prev.onclick = function() {GetInfoSequential('prev')};
+input.addEventListener('keydown', function(event) { if (event.key === "Enter") {GetInfoByInput()} })
+window.onload = function(){display.src = `${image_url}${1}.gif`; }
 
-//Declarando variaveis Publicas
+//Declarando variaveis da Função abaixo
 let pokemon_id = 1;
-
-async function GetInfoForPokemons(Next_or_Prev) {
+//Função
+async function GetInfoSequential(Next_or_Prev) {
    
     //Condições
     if (Next_or_Prev === 'next') {
@@ -26,7 +28,8 @@ async function GetInfoForPokemons(Next_or_Prev) {
         const resultado = await resposta.json()
 
         //Alterando HTML
-        PokemonName_display.innerText = resultado.forms[0].name
+        PokemonID_display.innerText = resultado.id
+        PokemonName_display.innerText = resultado.name
         display.src = `${image_url}${pokemon_id}.gif`
 
     } else if (Next_or_Prev === 'prev') {
@@ -38,13 +41,17 @@ async function GetInfoForPokemons(Next_or_Prev) {
         const resultado = await resposta.json()
 
         //Alterando HTML
-        PokemonName_display.innerText = resultado.forms[0].name
+        PokemonID_display.innerText = resultado.id
+        PokemonName_display.innerText = resultado.name
         display.src = `${image_url}${pokemon_id}.gif`
-        
+
     } else {
         console.error(`Error. input does recognized: ${Next_or_Prev}`)
         return
     }     
 }
-
-
+async function GetInfoByInput() {
+    const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value.toLowerCase()}`)
+    const resultado = await resposta.json()
+    PokemonName_display.innerText = resultado.name
+}
